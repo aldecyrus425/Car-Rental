526554a703c2d07c7528cafc2136b5510b8bcdae
Cyrus Alde <admin@dolephilschool.com>
2026-09-22 08:03:38 +0800

    Local Commit

--- MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
+++ MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc7648a7a75ff289520caf6d5b39d51ef9e040e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac61b89f9bc12dffce18307683bad062d88bb57")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
